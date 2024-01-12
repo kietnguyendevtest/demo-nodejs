b124846e49447f1073c7ca8f7521d062898b3c21
kietnguyendevtest <teiknguyen.work@gmail.com>
2024-01-12 16:59:09 +0700

    .

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
 require('dotenv').config();
 
+app.use(cors('*'));
 app.use(express.json());
 
 const bookRouter = require('./routes/book.router');
 const postRouter = require('./routes/post.router');
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 app.use('/api/v1/books', bookRouter);
 app.use('/api/v1/posts', postRouter);
