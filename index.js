0ab9d46ea840196d34e9d0ce69258e8b8320fd55
kietnguyendevtest <teiknguyen.work@gmail.com>
2024-01-12 15:53:00 +0700

    disable cors

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config();
+
 app.use(express.json());
 
 const bookRouter = require('./routes/book.router');
 const postRouter = require('./routes/post.router');
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/api/v1/books', bookRouter);
 app.use('/api/v1/posts', postRouter);
-
 
 app.listen(process.env.PORT, () => console.log(`Server on running on port: ${process.env.PORT}`))
